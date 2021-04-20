8773bb328be08e2bde96fa840288ec14df5cb134
ankiteric <ankitkapo@gmail.com>
2021-04-20 20:19:00 +0530

    Update M-AB.sh

--- M-AB.sh
+++ M-AB.sh
@@ -1,3 +1,21 @@
+/*****************************************************************************
+Copyright (c) 2021 M-AB
+
+All Rights Reserved.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2.0,
+as published by the Free Software Foundation.
+This program is also distributed with certain software (including
+but not limited to OpenSSL) that is licensed under separate terms,
+as designated in a particular file or component or in included license
+documentation.  The authors of M-AB hereby grant you an additional
+permission to link the program and your derivative works with the
+separately licensed software that they have included with M-AB.
+*****************************************************************************/
+
+
+### Written by Ankit Kapoor
+
 #!/bin/bash
 
 ### This script will install the mysqld from the binary files.User can download the binary files
